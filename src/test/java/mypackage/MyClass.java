45f32d98a9c09452a0b3f7805831486c26a98bbb
yuvrajgadadare <42262087+yuvrajgadadare@users.noreply.github.com>
2024-09-12 19:02:52 +0530

    Initial commit

---
+++ src/test/java/mypackage/MyClass.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class MyClass {
+
+	public void Addition() {
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("Add="+c);
+	}
+	public static void main(String[] args) {
+		
+	}
+}
