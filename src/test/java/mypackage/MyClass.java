ba757b91363769b2ab23a8f39bf5488b306f4de2
yuvrajgadadare <42262087+yuvrajgadadare@users.noreply.github.com>
2024-09-12 19:08:19 +0530

    fd

--- src/test/java/mypackage/MyClass.java
+++ src/test/java/mypackage/MyClass.java
@@ -5,7 +5,7 @@ public class MyClass {
 	public void Addition() {
 		int a=10,b=20,c;
 		c=a+b;
-		System.out.println("Add="+c)
+		System.out.println("Add="+c);
 	}
 	public static void main(String[] args) {
 		
